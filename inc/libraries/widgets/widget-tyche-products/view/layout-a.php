1dd4c74f243539aeed23087e1cda750c08204bb7
Cosmin <cristea.cosmin87@gmail.com>
2017-05-24 13:16:37 +0300

    Small fixes

--- inc/libraries/widgets/widget-tyche-products/view/layout-a.php
+++ inc/libraries/widgets/widget-tyche-products/view/layout-a.php
@@ -57,13 +57,13 @@ $posts = Tyche_Helper::get_products( $params ); ?>
 						</div>
 						<div class="tyche-product-body">
 							<h3><?php woocommerce_template_loop_product_link_open() ?><?php echo get_the_title(); ?><?php woocommerce_template_loop_product_link_close() ?></h3>
-
-							<?php if ( 'yes' === $params['show_rating'] && $rating_html = wc_get_rating_html( $product->get_average_rating() ) ) : ?>
+							<?php $rating_html = wc_get_rating_html( $product->get_average_rating() ); ?>
+							<?php if ( 'yes' === $params['show_rating'] && $rating_html ) : ?>
 								<?php echo $rating_html; ?>
 							<?php endif; ?>
 
-
-							<?php if ( $price_html = $product->get_price_html() ) : ?>
+							<?php $price_html = $product->get_price_html() ?>
+							<?php if ( $price_html ) : ?>
 								<span class="price"><?php echo $price_html; ?></span>
 							<?php endif; ?>
 
