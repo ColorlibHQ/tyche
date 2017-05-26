5e1d368a0c69c282d60e0b7c0de484d5c0c5a3fa
Cosmin <cristea.cosmin87@gmail.com>
2017-05-26 14:52:25 +0300

    more tests

--- build.sh
+++
@@ -1,9 +0,0 @@
-YEAR=$(date +"%Y")
-MONTH=$(date +"%m")
-git config --global user.email "${GIT_EMAIL}"
-git config --global user.name "${GIT_NAME}"
-git config --global push.default simple
-export GIT_TAG=V2.$YEAR-$MONTH.$TRAVIS_BUILD_NUMBER
-msg="Tag Generated from TravisCI for build $TRAVIS_BUILD_NUMBER"
-git add $TRAVIS_BUILD_DIR/build/tyche.zip
-git commit -m "Update build version file with $TRAVIS_BUILD_NUMBER"
