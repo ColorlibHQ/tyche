a1182b26bcf2abf1db7eb378f09f9c2a9cda29ef
Giucu <giucu91@yahoo.com>
2018-02-13 10:23:16 +0200

    #47

--- woocommerce/archive-product.php
+++ woocommerce/archive-product.php
@@ -10,10 +10,10 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see           https://docs.woocommerce.com/document/template-structure/
- * @author        WooThemes
- * @package       WooCommerce/Templates
- * @version       2.0.0
+ * @see 	    https://docs.woocommerce.com/document/template-structure/
+ * @author 		WooThemes
+ * @package 	WooCommerce/Templates
+ * @version     3.3.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
@@ -82,24 +82,24 @@ get_header( 'shop' ); ?>
 
 				<?php woocommerce_product_loop_start(); ?>
 
-				<?php woocommerce_product_subcategories(); ?>
+				<?php
+				if ( wc_get_loop_prop( 'total' ) ) {
+					while ( have_posts() ) {
+						the_post();
 
-				<?php while ( have_posts() ) : the_post(); ?>
+						/**
+						 * Hook: woocommerce_shop_loop.
+						 *
+						 * @hooked WC_Structured_Data::generate_product_data() - 10
+						 */
+						do_action( 'woocommerce_shop_loop' );
 
-					<?php
-					/**
-					 * woocommerce_shop_loop hook.
-					 *
-					 * @hooked WC_Structured_Data::generate_product_data() - 10
-					 */
-					do_action( 'woocommerce_shop_loop' );
-					?>
+						wc_get_template_part( 'content', 'product' );
+					}
+				}
 
-					<?php wc_get_template_part( 'content', 'product' ); ?>
-
-				<?php endwhile; // end of the loop. ?>
-
-				<?php woocommerce_product_loop_end(); ?>
+				woocommerce_product_loop_end();
+				?>
 
 				<?php
 				/**
@@ -110,13 +110,7 @@ get_header( 'shop' ); ?>
 				do_action( 'woocommerce_after_shop_loop' );
 				?>
 
-			<?php elseif ( ! woocommerce_product_subcategories(
-				array(
-					'before' => woocommerce_product_loop_start( false ),
-					'after'  => woocommerce_product_loop_end( false ),
-				)
-			)
-			) : ?>
+			<?php else : ?>
 
 				<?php
 				/**
